aa3c0e4d164d1ac49a0ed30eee19cf32b7000a2d
chanaya-ceuterick <chanaya.ceuterick@student.howest.be>
2024-12-08 20:35:18 +0100

    link to books-page

--- server/types/generated/contentTypes.d.ts
+++ server/types/generated/contentTypes.d.ts
@@ -493,6 +493,7 @@ export interface ApiCollectionCollection extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     description: Schema.Attribute.Text & Schema.Attribute.Required;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
